014a5b474a5683637ff1bbc8b01e541da3fb0a1c
tacogips <me@tacogips.me>
2023-03-29 14:47:54 +0900

    typo

--- crates/c1/src/lib.rs
+++ crates/c1/src/lib.rs
@@ -1,4 +1,4 @@
 pub use serde_json::{self, *};
 
-// It will result in an error if we use `use c1::serde_json` in c2/src/lib.rs while avoiding `pub use serde_json::self;` like belwo
+// It will result in an error if we call `use c1::serde_json` in c2/src/lib.rs while avoiding `pub use serde_json::self;` as below
 // pub use serde_json::*;
